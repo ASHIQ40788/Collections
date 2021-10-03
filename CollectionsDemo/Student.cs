4c1484f09a65e2e069dabe5bbac8362afa75cfb2
ShaikAshiq <ashiqshaiksvali8@gmail.comgit config --global user.email ashiqshaiksvali8@gmail.com>
2021-10-03 13:57:17 +0530

    [ASHIQ] added : collection program files

---
+++ CollectionsDemo/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionsDemo
+{
+    internal class Student
+    { 
+      public string FName { get; set; }
+        public string LName { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+
+    }
+}
